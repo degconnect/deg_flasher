df4c03769e728cefaca1328cc55d4670b5e1d6a2
Edilio Gallardo <edilio@degconnect.com>
2017-07-13 12:53:15 -0400

    saving backups to degconnect

--- comm.py
+++ comm.py
@@ -1,6 +1,13 @@
 import requests
 import urllib
 
+import os
+
+DOMAIN = os.environ.get('DOMAIN', 'flasher.degconnect.com')
+
+DEFAULT_USER = os.environ.get('DEFAULT_USER')
+DEFAULT_PASSWORD = os.environ.get('DEFAULT_PASSWORD')
+
 
 def get_modded(card):
     """
@@ -8,8 +15,8 @@ def get_modded(card):
     http://panels.degconnect.com/api/v1/core/rom-moddeds/?vram_size=4&memory=SK%20Hynix&rom_size=4&bios_version=113-1E366FU-S4J&model_1=RX%20580
     :return:
     """
-    domain = 'flasher.degconnect.com'
-    url = 'http://{}/api/v1/core/rom-moddeds/'.format(domain)
+
+    url = 'http://{}/api/v1/core/rom-moddeds/'.format(DOMAIN)
     params = {'vram_size': card['vram-size'], 'memory': card['memory'], 'rom_size': card['rom-size'],
               'bios_version': card['bios-version'], 'model_1': card['model_1']}
     resp = requests.get(url, params=params)
@@ -17,9 +24,28 @@ def get_modded(card):
         data = resp.json()
         if data:
             download_url = data[0]['download_url']
-            url = 'http://{}/{}'.format(domain, download_url)
+            url = 'http://{}/{}'.format(DOMAIN, download_url)
             lst = url.split('/')
             if lst:
                 name = 'modded-{}'.format(lst[-1])
                 urllib.urlretrieve (url, name)
                 return name
+
+
+def save_backup(filename, card):
+    url = 'http://{}/api/v1/core/user-backups/'.format(DOMAIN)
+    data = {'ssid': card['ssid'], 'generic_model': card['model'],
+            'vram_size': card['vram-size'], 'memory': card['memory'], 'rom_size': card['rom-size'],
+            'bios_version': card['bios-version'], 'model_1': card['model-1'], 'flash_type': card['flash-type'],
+           }
+    files = {'content': open(filename, 'rb')}
+    resp = requests.post(url, data=data, files=files, auth=(DEFAULT_USER, DEFAULT_PASSWORD), )
+    if resp.status_code == 200:
+        data = resp.json()
+        print data
+    elif resp.status_code == 500:
+        f = open('error.html', 'w')
+        f.write(resp.content)
+        f.close()
+    elif resp.status_code == 201:
+        print "Backup saved to Deconnect.com servers"
--- test_comm.py
+++ test_comm.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 
-from comm import get_modded
+from comm import get_modded, save_backup
 
-d = {'vram-size': 4, 'memory': 'SK Hynix', 'rom-size': 4, 'bios-version': '113-1E366FU-S4J', 'model_1': 'RX 580'}
 
-#             "model_1": "RX 580",
-#             "memory": "SK Hynix",
-#             "vram_size": 4,
-#             "rom_size": 4,
-#             "flash_type": "M25P20/c",
-#             "bios_version": "113-1E366FU-S4J"
+def test_get_modded():
 
-ret = get_modded(d)
+    d = {'vram-size': 4, 'memory': 'SK Hynix', 'rom-size': 4, 'bios-version': '113-1E366FU-S4J', 'model_1': 'RX 580'}
 
-print ret
+    #             "model_1": "RX 580",
+    #             "memory": "SK Hynix",
+    #             "vram_size": 4,
+    #             "rom_size": 4,
+    #             "flash_type": "M25P20/c",
+    #             "bios_version": "113-1E366FU-S4J"
+
+    ret = get_modded(d)
+
+    print ret
+
+
+def test_backup():
+    card = {'vram-size': 4, 'memory': 'SK Hynix', 'rom-size': 8, 'bios-version': '113-1E366FU-S4J', 'model_1': 'RX 580',
+            'ssid': '6361', 'generic_model': 'Ellesmere', 'flash-type': 'M25P20/c'}
+    save_backup('RX-580-4GB-SK-Hynix-4GB-ROM-113-1E366FU-S4J.rom', card)
+
+
+def main():
+    test_backup()
+
+
+if __name__ == "__main__":
+    main()
+
