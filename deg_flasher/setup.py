7cc6f2dcbcf604e191555cace386c057fe26fcf6
Edilio Gallardo <edilio@degconnect.com>
2017-07-14 13:13:24 -0400

    more into packaging the script to be run

--- deg_flasher/setup.py
+++ deg_flasher/setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(name='deg_flasher',
       version='0.1',
       description='A flasher utility for modding AMD cards',
-      url='http://github.com/storborg/funniest',
+      keywords='AMD Modded Radeon RX 470 480 570 580',
+      url='https://github.com/gdak/deg_flasher',
       author='DEGConnect',
       author_email='info@degconnect.com',
       license='MIT',
+      scripts=['bin/degflasher'],
       packages=['deg_flasher'],
       install_requires=[
           'requests==2.18.1',
